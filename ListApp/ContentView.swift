3f66d4358c790bbb0d7c8aca4963c195841b8919
Toshiyana <toshiyana0306@gmail.com>
2021-01-18 22:22:05 +0900

    Initial Commit

---
+++ ListApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListApp
+//
+//  Created by Toshiyana on 2021/01/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ListApp/ListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListAppApp.swift
+//  ListApp
+//
+//  Created by Toshiyana on 2021/01/18.
+//
+
+import SwiftUI
+
+@main
+struct ListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
